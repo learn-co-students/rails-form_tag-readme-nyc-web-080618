a84d41c37426c1a3cb017a176427669c1d700435
Yixuan Wang <ywang0506@gmail.com>
2018-08-31 12:34:29 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index]
+  resources :posts, only: [:index, :new, :create]
+  get "index", to: "posts#index"
+  get "new", to: "posts#new"
+  
 end
